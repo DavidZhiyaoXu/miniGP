ebe5ebc2bd4ac694f7b965afd71e2048400da9aa
DavidZhiyaoXu <xvzhiyao@outlook.com>
2024-09-12 00:07:43 -0400

    Cosmetic change to gp.py

--- miniGP/gp.py
+++ miniGP/gp.py
@@ -9,22 +9,21 @@ class GaussianProcess:
         self.X_train = None
         self.y_train = None
         self.K = None
-        self.noise = noise
         # TODO: Initialize field storing factorization of K_inverse
     
-    def fit(self, X_train: np.ndarray, y_train: np.ndarray) -> None:
-        self.X_train = torch.tensor(X_train)
-        self.y_train = torch.tensor(y_train)
-        self.K = self.kernel(self.X_train, self.X_train) + self.noise * torch.eye(len(self.X_train))
+    def fit(self, X_train: torch.tensor, y_train: torch.tensor) -> None:
+        self.X_train = X_train
+        self.y_train = y_train
+        self.K = self.kernel(self.X_train, self.X_train)
 
     # TODO: Should maintain a factorization of K_inverse. e.g. Cholesky + Schur Complement
-    def update(self, X_train: np.ndarray, y_train: np.ndarray) -> None:
+    def update(self, X_train: torch.tensor, y_train: torch.tensor) -> None:
         pass
 
-    def predict(self, X_test: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
+    def predict(self, X_test: torch.tensor) -> Tuple[torch.tensor, torch.tensor]:
         X_test_torch = torch.tensor(X_test)
         K_s = self.kernel(self.X_train, X_test_torch)
-        K_ss = self.kernel(X_test_torch, X_test_torch) + self.noise * torch.eye(len(X_test))
+        K_ss = self.kernel(X_test_torch, X_test_torch)
 
         # Inefficient computation of inverse. J
         K_inv = torch.inverse(self.K)
@@ -32,4 +31,4 @@ class GaussianProcess:
         mu_s = K_s.T @ K_inv @ self.y_train
         cov_s = K_ss - K_s.T @ K_inv @ K_s
         
-        return mu_s.detach().numpy(), cov_s.detach().numpy()
+        return mu_s, cov_s
